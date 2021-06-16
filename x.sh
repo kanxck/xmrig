003356bdced483ea95289e46766d3c78c2ed1370
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-06-17 04:19:00 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -4,8 +4,3 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/ck
 cd xmrig/src
 nano donate.h
-cd
-cd xmrig/ck
-cmake ..
-make -j$(nproc)
-./xmrig -o rx.unmineable.com:3333 -u SHIB:0xbcab043b1de28258230b01983b67ac99b522bd81.ide#vvsn-pz3n -p x 
