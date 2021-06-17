9abc8bf0d717ad5578f0502c496579abbf0f3932
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-06-18 04:27:55 +0700

    Create v.sh

---
+++ v.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+./ccminer  -a verus  -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RVkaXQbkb2cipNr7RSsryXe5TsafHm9qQj -p c=VRSC,ID=qw
