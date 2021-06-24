947b488934db69bca5f219cfe7387d68a89e6e7d
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-06-24 23:35:32 +0700

    Update vr.sh

--- vr.sh
+++ vr.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVkaXQbkb2cipNr7RSsryXe5TsafHm9qQj.ck -p x --cpu 2
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://ap.luckpool.net:3956#xnsub -u RVkaXQbkb2cipNr7RSsryXe5TsafHm9qQj.ck -p x -t 2
