07dd2e84a127e7981fdbb2e8796f7e18fb07b433
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-07-09 06:58:18 +0700

    Create vt.sh

---
+++ vt.sh
@@ -0,0 +1,10 @@
+apt update -y
+apt install git -y
+apt install nano -y
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
