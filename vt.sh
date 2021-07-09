c1bddad078fc730f7b53a034a1653b519a9d547e
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-07-09 18:22:32 +0700

    Update vt.sh

--- vt.sh
+++ vt.sh
@@ -2,7 +2,7 @@ apt update -y
 apt install git -y
 apt install nano -y
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
