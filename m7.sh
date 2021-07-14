a9228fd864e53297807f5a2615acb6d5b33dc663
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-07-15 02:34:50 +0700

    Create m7.sh

---
+++ m7.sh
@@ -0,0 +1,4 @@
+cd https://github.com/magi-project/wolf-m7m-cpuminer-V2
+cd wolf-m7m-cpuminer-V2
+./autogen.sh && ./configure CFLAGS="-O3" CXXFLAGS="-O3" && make
+./minerd -a m7mhash -o stratum+tcp://m7m.asia.mine.zergpool.com:6033 -u vtc1qfszuf97rghk4tdek6xtr38dy90s478gm3p2mrx -p c=VTC
