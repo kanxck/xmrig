8d20a673c1b03f9deaa7059906f6c23db9cf68c1
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-07-15 05:10:45 +0700

    Update m7.sh

--- m7.sh
+++ m7.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2.git
-apt install libgmp3-dev -
+apt install libgmp3-dev -y
 cd wolf-m7m-cpuminer-V2
 ./autogen.sh && ./configure CFLAGS="-O3" CXXFLAGS="-O3" && make
 ./minerd -a m7mhash -o stratum+tcp://m7m.asia.mine.zergpool.com:6033 -u vtc1qfszuf97rghk4tdek6xtr38dy90s478gm3p2mrx -p c=VTC
