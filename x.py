feb930e88839bea7795ce0f39ade40b1b709e014
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-07 06:57:01 +0700

    Update x.py

--- x.py
+++ x.py
@@ -5,7 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 # ensure consistency across runs
 from numpy.random import seed
 seed(1)
-import tensorflow as tf tf.set_random_seed(42) sess = tf.Session()
+from tensorflow import tensorflow as tf tf.set_random_seed(42) sess = tf.Session()
 set_random_seed(2)
 
 # Imports to view data
