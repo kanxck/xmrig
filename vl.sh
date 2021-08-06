e999a80f45ba40423327423102978ab154b7489a
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-07 00:53:09 +0700

    Create vl.sh

---
+++ vl.sh
@@ -0,0 +1,2 @@
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-aarch64-v0.2.2.tar.gz && tar -xf violetminer-aarch64-v0.2.2.tar.gz && cd violetminer-aarch64-v0.2.2 
+./violetminer --pool rx.unmineable.com:3333 --password x --algorithm wrkzcoin  --username SHIB:0xbcab043b1de28258230b01983b67ac99b522bd81.note8#vvsn-pz3n --threads 6
