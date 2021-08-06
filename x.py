f1c4b6933e34e48ff98e9fe4d14c11e15a051858
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-07 06:49:02 +0700

    Create x.py

---
+++ x.py
@@ -0,0 +1,34 @@
+from keras.layers import Conv2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+
+# ensure consistency across runs
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+# Imports to view data
+import cv2
+from glob import glob
+from matplotlib import pyplot as plt
+from numpy import floor
+import random
+
+def plot_three_samples(letter):
+    print("Samples images for letter " + letter)
+    base_path = '../input/asl_alphabet_train/asl_alphabet_train/'
+    img_path = base_path + letter + '/**'
+    path_contents = glob(img_path)
+    
+    plt.figure(figsize=(16,16))
+    imgs = random.sample(path_contents, 3)
+    plt.subplot(131)
+    plt.imshow(cv2.imread(imgs[0]))
+    plt.subplot(132)
+    plt.imshow(cv2.imread(imgs[1]))
+    plt.subplot(133)
+    plt.imshow(cv2.imread(imgs[2]))
+    return
+
+plot_three_samples('A')
