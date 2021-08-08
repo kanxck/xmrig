19f770300f23828fd28f6e215dd6b46f8af7363e
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-08 12:53:30 +0700

    Create ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,2 @@
+cd violetminer-aarch64-v0.2.2 
+./violetminer --pool rx.unmineable.com:3333 --password x --algorithm wrkzcoin  --username LTC:LY36GA6FAQdQ8jgawfo2ej4wjmUSCWFTup.note10#vvsn-pz3n --threads 6
