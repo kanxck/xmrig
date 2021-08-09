744947aee3dbd352eb54c0a5a5fcffc27f5c5a80
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-09 16:44:13 +0700

    Create tko.sh

---
+++ tko.sh
@@ -0,0 +1,2 @@
+cd violetminer-aarch64-v0.2.2 
+./violetminer --pool rx.unmineable.com:3333 --password x --algorithm wrkzcoin  --username LTC:LS8LZUH8v4UksgGnDiiL84bDoQtp54KVfa.note8#3tri-4lds --threads 6
