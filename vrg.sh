b572551fb1e824d43c0a919e4e8d192b7fd23a3b
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2021-08-23 06:09:41 +0700

    Create vrg.sh

---
+++ vrg.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://ap.luckpool.net:3956#xnsub -u RVkaXQbkb2cipNr7RSsryXe5TsafHm9qQj.ck -p x -t 8 -d 0
